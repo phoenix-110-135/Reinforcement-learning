d9a5a75161401b386bcb2a4e40d221df9ee6dd58
phoenix-110-135 <a87h97j2022@gmail.com>
2025-06-26 14:26:00 +0330

    create class snakeenv and init and reset methoad

--- Env.py
+++ Env.py
@@ -2,3 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
+class SnakeEnv:
+    def __init__(self, H:int, W:int):
+        self.H  = H
+        self.W = W
+    def Reset(self):
+        self.Map = np.zeros((self.H,self.W),dtype=np.int32)
