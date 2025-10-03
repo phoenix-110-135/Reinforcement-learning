8b12e16e4f0695f590751468039586fc2cbe452e
phoenix-110-135 <a87h97j2022@gmail.com>
2025-10-03 21:38:26 +0330

    create class of SnakeAgent

--- agent.py
+++ agent.py
@@ -7,3 +7,15 @@ import keras.layers as lay
 import keras.activations as act 
 import keras.optimizers as opt 
 import keras.losses as los 
+
+class SnakeAgent:
+    def __init__(self,
+                 nEpsilon:int=100,
+                 Epsilon1:float=0.99,
+                 Epsilon2:float=0.01):
+        self.nEpsilon = nEpsilon
+        self.Epsilon1 = Epsilon1
+        self.Epsilon2 = Epsilon2
+        self.Epsilons = np.linspace(start=Epsilon1,
+                                    stop=Epsilon2,
+                                    num=nEpsilon)
