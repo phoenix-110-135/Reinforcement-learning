33b57a0a5fea3c53f380da247cf360db812022d9
phoenix-110-135 <a87h97j2022@gmail.com>
2025-10-03 21:32:03 +0330

    require Libraries

---
+++ agent.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import matplotlib.pyplot
+import tensorflow as tf 
+import keras.models as mod 
+import tensorflow as tf
+import keras.layers as lay 
+import keras.activations as act 
+import keras.optimizers as opt 
+import keras.losses as los 
