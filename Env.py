7cdeaf369f34c0dce2d665b58fdca242cbd6c0eb
phoenix-110-135 <a87h97j2022@gmail.com>
2026-02-26 21:44:21 +0330

    complite env

--- Env.py
+++ Env.py
@@ -1,69 +1,62 @@
-import numpy as np 
-import matplotlib.pyplot  as plt  
-from matplotlib.colors import  ListedColormap  
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 class SnakeEnv:
     def __init__(self, H:int, W:int):
-        self.H  = H
+        self.H = H
         self.W = W
-        self.object2code = {'Free':0,
-                            'Snake':1,
-                            'Food':2,
-                            'Head':3,
-                            'Out':4}
-        self.Action2Code = {'Up':0,
-                            'Right':1,
-                            'Down':2,
-                            'Left':3}
-        self.Action2Trans = {"Up":np.array([-1,0]),
-                             "Right":np.array([0,+1]),
-                             "Down":np.array([+1,0]),
-                             "Left":np.array([0,-1])}
-        
+        self.Object2Code = {'Free': 0,
+                            'Snake': 1,
+                            'Food': 2,
+                            'Head': 3,
+                            'Out': 4}
+        self.t1 = [0, 1, 2, 4]
+        self.Action2Code = {'Up': 0,
+                            'Right': 1,
+                            'Down': 2,
+                            'Left': 3}
+        self.Code2Trans = {0: np.array([-1, 0]),
+                           1: np.array([0, +1]),
+                           2: np.array([+1, 0]),
+                           3: np.array([0, -1])}
+        self.Rewards = {'Food': +12,
+                        'Snake': -11,
+                        'Out': -10,
+                        'Closer': +1.5,
+                        'Reverse': -1}
         self.nAction = len(self.Action2Code)
         self.Reset()
-
-
     def Reset(self):
-        self.Map = np.zeros((self.H,self.W),dtype=np.int8)
+        self.Map = np.zeros((self.H, self.W), dtype=np.int8)
         self.ResetFood()
         self.ResetSnake()
-
     def ResetFood(self):
-        m = self.Map == self.object2code['Food']
-        self.Map[m] = self.object2code['Free']
+        m = self.Map == self.Object2Code['Food']
+        self.Map[m] = self.Object2Code['Free']
         h = np.random.randint(low=0, high=self.H)
         w = np.random.randint(low=0, high=self.W)
-        while self.Map[h,w] != self.object2code['Free']:
+        while self.Map[h, w] != self.Object2Code['Free']:
             h = np.random.randint(low=0, high=self.H)
             w = np.random.randint(low=0, high=self.W)
-        self.Map[h,w] = self.object2code['Food']
-        self.Food = np.array([w,h])
-
+        self.Map[h, w] = self.Object2Code['Food']
+        self.Food = np.array([h, w])
     def ResetSnake(self):
-        m1 = self.Map == self.object2code['Snake']
-        m2 = self.Map == self.object2code['Head']
-        self.Map[m1] = self.object2code['Free']
-        self.Map[m2] = self.object2code['Free']
+        m1 = self.Map == self.Object2Code['Snake']
+        m2 = self.Map == self.Object2Code['Head']
+        self.Map[m1] = self.Object2Code['Free']
+        self.Map[m2] = self.Object2Code['Free']
         h = np.random.randint(low=0, high=self.H)
         w = np.random.randint(low=0, high=self.W)
-        while self.Map[h,w] != self.object2code['Free']:
+        while self.Map[h, w] != self.Object2Code['Free']:
             h = np.random.randint(low=0, high=self.H)
             w = np.random.randint(low=0, high=self.W)
-        self.Map[h,w] = self.object2code['Head']
-        self.Head = np.array([w,h])
+        self.Map[h, w] = self.Object2Code['Head']
+        self.Head = np.array([h, w])
         self.Snake = []
-
-    def show(self):
-        colors = ['black', 'orange','yellow','purple']  
-
-        cmap = ListedColormap(colors)
-
-        plt.imshow(self.Map, cmap=cmap)
+    def Show(self):
+        plt.imshow(self.Map)
         plt.show()
-
-Env = SnakeEnv(9,14)
-Env.show()
-
-print("end")
+    def OnlineShow(self):
+        plt.cla()
+        plt.imshow(self.Map)
+        plt.pause(0.05)
